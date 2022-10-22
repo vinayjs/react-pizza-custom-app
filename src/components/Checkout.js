c6fbb5dad2809c1d43dbd1754e7e0705698de257
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-10-22 17:05:01 +0530

    React project - pizza customization

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-dom";
 import Pizzaman from "../assets/PizzaMan.png";
 
 export default function Checkout({ ingredients }) {
